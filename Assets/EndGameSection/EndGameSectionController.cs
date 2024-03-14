295ca062429a756e9f6c0f090c163fdb3a827de7
Eray Yuksel <yukseleray203@gmail.com>
2024-03-14 15:05:14 +0300

    AddedPlayerPrefsList

--- Assets/EndGameSection/EndGameSectionController.cs
+++ Assets/EndGameSection/EndGameSectionController.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class EndGameSectionController : MonoBehaviour
 {
-    private bool control = false;
+    [SerializeField] List<GameObject> labRoomListFalse = new List<GameObject>();
+    [SerializeField] List<GameObject> labRoomListTrue = new List<GameObject>();
+    [SerializeField] List<PlayerPrefs> labRoomSaveList = new List<PlayerPrefs>();
+
+    private void Start()
+    {
+        GetPlayerPrefsList();
+    }
+    void ActivatingRandomLabStuff()
+    {
+        int randomNumber = Random.Range(0, labRoomListFalse.Count);
+        var obj = labRoomListFalse[randomNumber];
+        SavePlayerPrefsList(randomNumber);
+        labRoomListFalse.Remove(obj);
+        labRoomListTrue.Add(obj);
+        obj.SetActive(true);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (control) return;
-            control = true;
+            ActivatingRandomLabStuff();
             var playerController = other.GetComponent<PlayerController>();
             playerController.endSectionModule.PlayerEndSectionMovement();
+        }
+    }
+    void SavePlayerPrefsList(int objIndex)
+    {
+        for (int i = 0; i < labRoomListTrue.Count; i++)
+        {
+            PlayerPrefs.SetInt("obj" + i, objIndex);
+        }
+    }
+    public void GetPlayerPrefsList()
+    {
+        for (int i = 0; i < labRoomListTrue.Count; i++)
+        {
+            int objIndex = PlayerPrefs.GetInt("obj" + i);
+            labRoomListTrue[objIndex].SetActive(true);
         }
     }
 }
