96a25b0a8208219ce1d6eb5c1a4a6d79f64d9b40
Eray Yuksel <yukseleray203@gmail.com>
2024-03-30 15:42:33 +0300

    AddedShaderGraphs

--- Assets/UIAssets/CureProgressController.cs
+++ Assets/UIAssets/CureProgressController.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CureProgressController : MonoBehaviour
 {
     [SerializeField] Image slider;
-    
-
+    [SerializeField] TextMeshProUGUI multipleText;
+    int multipleTextValue = 1;
+    float recordedCureSliderValue = 0;
+    public void WriteMultipleText()
+    {
+        multipleTextValue++;
+        multipleText.text = "x" + multipleTextValue.ToString();
+    }
     public void CureSliderIncrease(float increaseValue)
     {
         slider.fillAmount += increaseValue;
+        if (slider.fillAmount >= 0.99f)
+        {
+            recordedCureSliderValue += slider.fillAmount; // bu deger en son beherglass da artacak olan cure sivisininin miktarini belirleyecek
+            slider.fillAmount = 0;
+            WriteMultipleText();
+        }
     }
 
 }
